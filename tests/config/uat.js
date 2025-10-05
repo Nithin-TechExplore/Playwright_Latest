a21b592c9d9a04ecb8652bd97c54c97ba66264b6
varun <varunn068@gmail.com>
2025-10-05 16:47:35 +0530

    Added credentials in vaults

--- tests/config/uat.js
+++ tests/config/uat.js
@@ -1,6 +1,5 @@
 module.exports = {
     baseUrl: 'https://www.saucedemo.com/',
-    userName:  "c3RhbmRhcmRfdXNlcg==",
-    password:  "c2VjcmV0X3NhdWNl"
-
+    userName: process.env.TEST_USERNAME || 'standard_user',
+    password: process.env.TEST_PASSWORD || 'secret_sauce'
 }
--- tests/support/custom.lib.js
+++ tests/support/custom.lib.js
@@ -4,9 +4,9 @@ const data = require('../../tests/config/' + env.toLowerCase());
 
 const custom = {
     enterCredntials: async(data) => {
-        await actions.enterValue("//input[@id='user-name']",atob(data['userName']))
+        await actions.enterValue("//input[@id='user-name']",(data['userName']))
         await page.waitForTimeout(2000);
-        await actions.enterValue("//input[@id='password']",atob(data['password']))
+        await actions.enterValue("//input[@id='password']",(data['password']))
         await page.waitForTimeout(2000);
         await actions.clickOnElement("//input[@id='login-button']");
         await page.waitForTimeout(2000);
